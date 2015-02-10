f9c0c2ceaa7411181a2631c96c610a77e41ae9f6
y62wang <y62wang@gmail.com>
2015-02-09 19:15:06 -0800

    add basic node classes for future exercises

---
+++ src/com/y62wang/learn/basic/BinaryTreeNode.java
@@ -0,0 +1,34 @@
+package com.y62wang.learn.basic;
+
+/**
+ * A class that represents a Binary Tree Node
+ */
+public class BinaryTreeNode<T> {
+    private BinaryTreeNode<T> left;
+    private BinaryTreeNode<T> right;
+    private T value;
+
+    public BinaryTreeNode<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(BinaryTreeNode<T> left) {
+        this.left = left;
+    }
+
+    public BinaryTreeNode<T> getRight() {
+        return right;
+    }
+
+    public void setRight(BinaryTreeNode<T> right) {
+        this.right = right;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+}
